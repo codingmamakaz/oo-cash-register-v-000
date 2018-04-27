091092511aa4331a15fbadabbb7eb53ad3f07e5e
Kazumi Karbowski <kazkazinus@gmail.com>
2018-04-27 17:48:31 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,38 @@
+require'pry'
 
+class CashRegister
+  attr_accessor :total, :discount, :title, :price, :quantity
+  @@all = []
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(title, price, quantity = 1)
+    @@all << title
+    @total = @total + (price * quantity)
+  end
+
+  def apply_discount
+    if @discount.equal?(0)
+      "There is no discount to apply."
+    elsif
+    @total = @total * 0.8
+    "After the discount, the total comes to $#{@total.round}."
+    end
+  end
+
+  def items
+    @@all.flatten
+
+  end
+
+    # def void_last_transaction
+    #   #subtracts the last transaction from the total
+    #   self.add_item(title, price, quantity = 1).map do |t,p,q|
+    #   @total - p[-1]
+    # end
+    # end
+
+end
